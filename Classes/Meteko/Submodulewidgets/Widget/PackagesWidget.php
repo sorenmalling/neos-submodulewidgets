67ead8e2a5eb9d11d75e9d51468992e3a2ef4a52
Soren Malling <soren.malling@gmail.com>
2014-02-13 19:47:57 +0100

    [TASK] Example package submodule widget

---
+++ Classes/Meteko/Submodulewidgets/Widget/PackagesWidget.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Meteko\Submodulewidgets\Widget;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Fluid\View\StandaloneView;
+use TYPO3\Neos\Controller\Module\SubmoduleWidgetInterface;
+
+class PackagesWidget implements SubmoduleWidgetInterface {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Package\PackageManagerInterface
+	 */
+	protected $packageManager;
+
+	/**
+	 * Render submodule widget content
+	 *
+	 * @param array $widgetConfiguration Submodule configuration
+	 *
+	 * @return string
+	 */
+	public function render($widgetConfiguration) {
+		$availablePackages = $this->packageManager->getAvailablePackages();
+		$packages = array();
+		/** @var $package \TYPO3\Flow\Package\PackageInterface */
+		foreach ($availablePackages as $package) {
+			$packages[$package->getPackageKey()] = array(
+				'isActive' => $this->packageManager->isPackageActive($package->getPackageKey()),
+				'name' => $package->getComposerManifest('name')
+			);
+		}
+		$view = new StandaloneView();
+		$view->setTemplatePathAndFilename('resource://Meteko.Submodulewidgets/Private/Templates/Widget/Packages.html');
+		$view->assign('packages', $packages);
+		return $view->render();
+	}
+
+}
